b7f620e4618c0853a9cee57f0e72e8b5796bc599
Clement LAMBERT <clement.lambert@etudiant.univ-lille1.fr>
2016-01-26 11:23:22 +0100

    Renaming server_socket by sockfd for clarity

--- webserver/socket.c
+++ webserver/socket.c
@@ -2,37 +2,42 @@
 
 int create_socket(void)
 {
-	int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_socket == -1)
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1)
 	{
 		perror("Creating serveur socket:");
 		return -1;
 	}
-	return server_socket;
+	return sockfd;
 }
 
-int bind_socket(int server_socket, int port)
+int bind_socket(int sockfd, int port)
 {	
 	struct sockaddr_in saddr;
 	saddr.sin_family = AF_INET;
 	saddr.sin_port = htons(port);
 	saddr.sin_addr.s_addr = INADDR_ANY;
 
-	if (bind(server_socket,(struct sockaddr *)&saddr, sizeof(saddr)) == -1)
+	if (bind(sockfd,(struct sockaddr *)&saddr, sizeof(saddr)) == -1)
 	{
 		perror("Binding server socket address:");
 		return -1;
 	}
-	return server_socket;
+	return sockfd;
 }
 
 int creer_serveur(int port)
 {
-	int server_socket;
-	if ((server_socket = create_socket()) == -1)
+	int sockfd;
+	if ((sockfd = create_socket()) == -1)
 		return -1;
-	if (bind_socket(server_socket, port) == -1)
+	if (bind_socket(sockfd, port) == -1)
 		return -1;
 	
-	return server_socket;
+	return sockfd;
+}
+
+int listen(int sockfd, int backlog_size)
+{
+
 }
